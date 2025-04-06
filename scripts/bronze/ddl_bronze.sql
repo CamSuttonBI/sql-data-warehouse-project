9e951d95eab3e292a9afe5fa3063434ae1a71ad2
CamSuttonBI <csuttonanalytics@gmail.com>
2025-04-06 08:57:10 -0500

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,6 +1,6 @@
 	/*
 	===============================================================================
-	DDL Script: Create Broze Tables
+	DDL Script: Create Bronze Tables
 	===============================================================================
 	Script Purpose:
 		This script creates tables in the 'bronze' schema, dropping existing tables 
